fdbb88b0a251e35132386cbfa43091fccddb135c
mohammadreza rezaei <rezaeim748@gmail.com>
2020-11-29 21:06:22 +0330

    adding classes

---
+++ src/com/company/StudentProfile.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import javax.swing.*;
+
+public class StudentProfile {
+    private JFrame studentFrame ;
+    private JPanel column ;
+    private JPanel personal ;
+    private JPanel classes ;
+    private JPanel account ;
+    private JPanel mealReserve ;
+    private JPanel classChoosing ;
+
+
+    public StudentProfile (){
+        studentFrame = new JFrame() ;
+        column = new JPanel() ;
+        personal = new JPanel() ;
+        classes = new JPanel() ;
+        account = new JPanel() ;
+        mealReserve = new JPanel() ;
+        classChoosing = new JPanel() ;
+
+
+
+
+    }
+
+
+
+
+
+
+}
